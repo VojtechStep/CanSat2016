db6409649b6e1513b0835f7d6b42d695a4861882
Jan Šimek <honzasima@hotmail.cz>
2016-02-15 12:30:27 +0000

    git-tfs-id: [https://vojtechstepancik.visualstudio.com/defaultcollection]$/CanSat/CanSat2016;C58

--- ProbeCode/ProbeCode.ino
+++ ProbeCode/ProbeCode.ino
@@ -5,11 +5,37 @@
 */
 
 // the setup function runs once when you press reset or power the board
-void setup() {
+#include <RFM69registers.h>
+#include <RFM69.h>
+#include <qbcan.h>
+#include <BMP180.h>
 
+BMP180 bmp;
+double T, P;
+
+long startMillis;
+long timeout = 10000;
+
+char data[50];
+
+void setup()
+{
+	Serial.begin(9600);
+	bmp.begin();
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-  
+void loop()
+{
+	Serial.println("START");
+	startMillis = millis();
+	while (millis() < startMillis + timeout)
+	{
+		bmp.getData(T, P);
+
+		sprintf(data, "%d,%d,1000,1000,1000,A,1000000,N,1000000,W", T * 1000, P * 10);
+		Serial.println(data);
+		delay(100);
+	}
+	Serial.println("PAUSE");
+	delay(5000);
 }
