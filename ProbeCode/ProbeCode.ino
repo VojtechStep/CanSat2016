b643c7fb61b8b443a5bdcf9ff38ccd5f6d2a3d1b
James Bohacek <jamesbohacek@outlook.com>
2016-03-29 06:55:27 +0000

    WTF is happening part 2
    git-tfs-id: [https://vojtechstepancik.visualstudio.com/defaultcollection]$/CanSat/CanSat2016;C104

--- ProbeCode/ProbeCode.ino
+++ ProbeCode/ProbeCode.ino
@@ -1,44 +1,23 @@
 ﻿/*
- Name:		ProbeCode.ino
- Created:	2/13/2016 9:43:30 PM
- Author:	Vojtěch && Jacob (Vojtěch is first, because he actually works)
-
- SD Write speed: 1Mb / 48508mS = 0.0206 Mb/S
-
- Serial: PC
- Serial1: GPS
- Serial2: Camera subsystem
-
- Packet Structure:
- **|Type|**|Name|******|Unit|*********|Data Decomposition|*****************************************************|Length|*|Comment|************
- *  Byte	PacketStart	[]			= 0x08																		1b							*
- *	Single	UTCTime		[hhmmss.sss]= (data[ 1] << 24) | (data[ 2] << 16) | (data[ 3] << 8) | (data[ 4] << 0)	4b							*
- *	Single	Temperature [°C]		= (data[ 5] << 24) | (data[ 6] << 16) | (data[ 7] << 8) | (data[ 8] << 0)	4b							*
- *	Single	Pressure	[mB]		= (data[ 9] << 24) | (data[10] << 16) | (data[11] << 8) | (data[12] << 0)	4b							*
- *	Short	XAccRaw		[1]			=										(data[13] << 8) | (data[14] << 0)	2b		xg = x * Grange/1024*
- *	Short	YAccRaw		[1]			=										(data[15] << 8) | (data[16] << 0)	2b		yg = y * Grange/1024*
- *	Short	ZAccRaw		[1]			=										(data[17] << 8) | (data[18] << 0)	2b		zg = z * Grange/1024*
- *	Single	Latitude	[°"]		= (data[19] << 24) | (data[20] << 16) | (data[21] << 8) | (data[22] << 0)	4b							*
- *	Char	NSIndicator	[]			=														  (data[23] << 0)	1b		North / South		*
- *	Single	Longitude	[°"]		= (data[24] << 24) | (data[25] << 16) | (data[26] << 8) | (data[27] << 0)	4b							*
- *	Char	EWIndicator	[]			=														  (data[28] << 0)	1b		East  / West		*
- *	Single	MSLAltitude	[m]			= (data[29] << 24) | (data[30] << 16) | (data[31] << 8) | (data[32] << 0)	4b							*
- *  Byte	PacketEnd	[]			= 0x09																		1b							*
- ********************************************************************************************************************************************
+Name:		CompanionCode.ino
+Created:	3/9/2016 9:50:59 AM
 */
+
+#include <SoftwareSerial.h>
+#include "ADXL345.h"
+#include <RFM69registers.h>
+#include <RFM69.h>
+#include <qbcan.h>
+#include <BMP180.h>
+#include <SD.h>
+
+#define LOG(x) Serial.println(x)
 
 /*
-	Serial - PC / Companion -- 115200
-	Serial1 - GPS -- 4800
+Serial - Debug/Probe -- 115200
+Serial1 - GPS
+Serial2 - Camera
 */
-
-//#include "ADXL345.h"
-#include <Wire.h>
-#include <RFM69.h>			//Radio
-#include <BMP180.h>			//Temp & Pres
-
-#define BUFFER_LENGTH 64
-
 
 #define Serial3Implemented
 #define RFM_CS_PIN 10
@@ -50,8 +29,8 @@
 #define GATEWAYID 12
 #define ENCRYPTKEY "AlmightyLobsters"
 
-bool sending;
-bool repeat;
+bool sending = true;
+bool repeat = true;
 byte inByte;
 byte gpsInByte;
 String gpsIn;
@@ -72,29 +51,124 @@ const byte takeImageCommand = 0x6A;
 
 BMP180 bmp;
 ADXL345 adxl;
-RFM69 radio(RFM_CS_PIN, RFM_INT_PIN, true, RFM_INT_NUM);
 
-char payload[BUFFER_LENGTH];
+File logFile;
+
+/*camera definitions*/
+byte ZERO = 0x00;
+byte incomingbyte;
+byte a[32];
+long int j = 0, k = 0, count = 0, i = 0x0000;
+uint8_t MH, ML;
+boolean EndFlag = 0;
+int iter = 0, l;
+File camFile;
+/********************/
+
+unsigned long cur = 0, last = 0;
+int delta = 0;
+
+long iteration = 0;
+
+/******functions******/
+void SendResetCmd();
+void SetImageSizeCmd(byte Size);
+void SetBaudCmd(byte baudrate);
+void SendTakePhotoCmd();
+void SendReadDataCmd();
+void StopTakePhotoCmd();
+/**********************/
+
 
 void setup()
 {
+	Serial.begin(115200);
+	pinMode(10, OUTPUT);
+	if (!SD.begin(4))
+	{
+		Serial.println("SD not found.");
+		while (1);
+	}
 	Serial1.begin(4800);
-	Serial2.begin(9600);
+	Serial2.begin(115200);
 	bmp.begin();
 	adxl.begin();
-	adxl.setRange(3); //RNG_16G
-	radio.initialize(FREQUENCY, NODEID, NETWORKID);
-	radio.setHighPower();
-	radio.encrypt(ENCRYPTKEY);
-	radio.send(GATEWAYID, &broadcastInitMessage, 1);
+	adxl.setRange(3);
+	Serial.write(broadcastInitMessage);
+	delay(100);
+	SendResetCmd();
+	delay(2000);
+	SetBaudCmd(0x2A);
+	delay(500);
+	Serial2.begin(38400);
+	delay(100);
 }
 
+// the loop function runs over and over again until power down or reset
 void loop()
 {
-//Read Input
-	if(radio.receiveDone())
+	Serial.println("Start of an iteration.");
+	EndFlag = 0;
+	SendResetCmd();
+	delay(3000);
+	//Camera set, start receiving data
+	LOG("Shooting img...");
+	SendTakePhotoCmd();
+	delay(3000);
+	while (Serial2.available() > 0)
 	{
-		inByte = radio.DATA[0];
+		incomingbyte = Serial2.read();
+	}
+	Serial.println("Creating file...");
+	String name = "pic" + String(iteration) + ".jpg";
+	while (SD.exists(name) && name.length() < 12) name = name.substring(0, name.length() - 4) + "n" + ".jpg";
+	if (SD.exists(name))	SD.remove(name);
+	camFile = SD.open(name, FILE_WRITE);
+	LOG("Starting subMainLoop...");
+	while (!EndFlag)
+	{
+		mainLoopThrough();
+		j = 0;
+		k = 0;
+		count = 0;
+		SendReadDataCmd();
+		delay(20);
+		while (Serial2.available() > 0)
+		{
+			incomingbyte = Serial2.read();
+			Serial.print(String(incomingbyte) + " ");
+			k++;
+			delay(1); //250 for regular
+			if ((k > 5) && (j < 32) && (!EndFlag))
+			{
+				a[j] = incomingbyte;
+				if ((a[j - 1] == 0xFF) && (a[j] == 0xD9))     //tell if the picture is finished
+				{
+					EndFlag = 1;
+				}
+				j++;
+				count++;
+			}
+		}
+		Serial.println();
+		for (l = 0; l < count; l++)
+			camFile.write(a[l]);
+	}
+	LOG("Final check");
+	iteration++;
+	if (iteration > 2) {
+		Serial.println("The end.");
+		while (1);
+	}
+	LOG("Check donne!");
+}
+
+
+void mainLoopThrough() {
+	//Computer In
+	while (Serial.available() > 0)
+	{
+		inByte = Serial.read();
 		if (inByte == startMesCommand)
 		{
 			sending = true;
@@ -111,76 +185,42 @@ void loop()
 			repeat = false;
 		}
 	}
-	
-	//Get GPS
-	gpsIn = "";
 
-	while (gpsIn.length() < 6 || gpsIn.substring(0, 6) != "$GPGGA")
+	// GPS
+	if (Serial1.available() > 0)
 	{
-		gpsInByte = Serial1.read();
+		inByte = Serial1.read();
 		if ((char)inByte == '$')
 		{
-			gpsIn = (char)gpsInByte + Serial1.readStringUntil('\r');
+			gpsIn = Serial1.readStringUntil('\r');
+			if (split(gpsIn, ',', 0) == "GPGGA") {
+				if (sending) {
+					sending = repeat;
+					double utcTime = atof(split(gpsIn, ',', 1).c_str());
+					double temp, pres;
+					bmp.getData(temp, pres);
+					short xacc, yacc, zacc;
+					adxl.readAcceleration(&xacc, &yacc, &zacc);
+					xacc *= 16 / 1024;
+					yacc *= 16 / 1024;
+					zacc *= 16 / 1024;
+					double lat = atof(split(gpsIn, ',', 2).c_str());
+					char ns = split(gpsIn, ',', 3)[0];
+					double lon = atof(split(gpsIn, ',', 4).c_str());
+					char ew = split(gpsIn, ',', 5)[0];
+					double alt = atof(split(gpsIn, ',', 9).c_str());
 
+					String msg = String(utcTime) + "," + String(temp) + "," + String(pres) + "," + String(xacc) + "," + String(yacc) + "," + String(zacc) + "," + String(lat) + "," + String(ns) + "," + String(lon) + "," + String(ew) + "," + String(alt);
+
+					Serial.write(packetStartMessage);
+					Serial.print(msg);
+					Serial.write(packetEndMessage);
+					logFile = SD.open("data.log", FILE_WRITE);
+					logFile.print(msg);
+					logFile.close();
+				}
+			}
 		}
-	}
-
-	//Write Output
-	if (sending)
-	{
-		sending = repeat;
-		//Serial.write(mesurementStartMessage);
-		//Define mes variables
-		double utcTime;
-		double temp;
-		double pres;
-		short xacc;
-		short yacc;
-		short zacc;
-		double lat;
-		char ns;
-		double lon;
-		char ew;
-		double alt;
-
-		
-
-		//Get other values
-		utcTime = atof(split(gpsIn, ',', 1).c_str());
-		bmp.getData(temp, pres);
-		adxl.readAcceleration(&xacc, &yacc, &zacc);
-		xacc *= 16 / 1024;
-		yacc *= 16 / 1024;
-		zacc *= 16 / 1024;
-		lat = atof(split(gpsIn, ',', 2).c_str());
-		ns = split(gpsIn, ',', 3)[0];
-		lon = atof(split(gpsIn, ',', 4).c_str());
-		ew = split(gpsIn, ',', 5)[0];
-		alt = atof(split(gpsIn, ',', 9).c_str());
-
-		//Join them together
-		String msg = String(utcTime) + "," + String(temp) + "," + String(pres) + "," + String(xacc) + "," + String(yacc) + "," + String(zacc) + "," + String(lat) + "," + String(ns) + "," + String(lon) + "," + String(ew) + "," + String(alt);
-
-
-
-		//Write Packet Start
-		payload[0] = packetStartMessage;
-		Serial2.write(packetStartMessage);
-		//Write packet
-		for (int i = 0; i < BUFFER_LENGTH - 3; i++)
-		{
-			payload[i + 1] = msg[i];
-		}
-		radio.send(GATEWAYID, payload, BUFFER_LENGTH);
-		Serial2.print(msg);
-		//Write Packet End
-		payload[BUFFER_LENGTH - 1] = packetEndMessage;
-		Serial2.write(packetEndMessage);
-
-		//Write pause or end
-		//Serial.write(sending ? mesurementPauseMessage : mesurementEndMessage);
-		//Serial2.write(sending ? mesurementPauseMessage : mesurementEndMessage);
-
 	}
 }
 
@@ -200,4 +240,92 @@ String split(String data, char separator, int index)
 		}
 	}
 	return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+
+void SendResetCmd()
+{
+	Serial2.write(0x56);
+	Serial2.write(ZERO);
+	Serial2.write(0x26);
+	Serial2.write(ZERO);
+}
+
+/*************************************/
+/* Set ImageSize :
+/* <1> 0x22 : 160*120
+/* <2> 0x11 : 320*240
+/* <3> 0x00 : 640*480
+/* <4> 0x1D : 800*600
+/* <5> 0x1C : 1024*768
+/* <6> 0x1B : 1280*960
+/* <7> 0x21 : 1600*1200
+/************************************/
+void SetImageSizeCmd(byte Size)
+{
+	Serial2.write(0x56);
+	Serial2.write(ZERO);
+	Serial2.write(0x54);
+	Serial2.write(0x01);
+	Serial2.write(Size);
+}
+
+/*************************************/
+/* Set BaudRate :
+/* <1> 0xAE  :   9600
+/* <2> 0x2A  :   38400
+/* <3> 0x1C  :   57600
+/* <4> 0x0D  :   115200
+/* <5> 0xAE  :   128000
+/* <6> 0x56  :   256000
+/*************************************/
+void SetBaudCmd(byte baudrate)
+{
+	Serial2.write(0x56);
+	Serial2.write(ZERO);
+	Serial2.write(0x24);
+	Serial2.write(0x03);
+	Serial2.write(0x01);
+	Serial2.write(baudrate);
+}
+
+void SendTakePhotoCmd()
+{
+	Serial2.write(0x56);
+	Serial2.write(ZERO);
+	Serial2.write(0x36);
+	Serial2.write(0x01);
+	Serial2.write(ZERO);
+}
+
+void SendReadDataCmd()
+{
+	MH = i / 0x100;
+	ML = i % 0x100;
+	Serial2.write(0x56);
+	Serial2.write(ZERO);
+	Serial2.write(0x32);
+	Serial2.write(0x0c);
+	Serial2.write(ZERO);
+	Serial2.write(0x0a);
+	Serial2.write(ZERO);
+	Serial2.write(ZERO);
+	Serial2.write(MH);
+	Serial2.write(ML);
+	Serial2.write(ZERO);
+	Serial2.write(ZERO);
+	Serial2.write(ZERO);
+	Serial2.write(0x20);
+	Serial2.write(ZERO);
+	Serial2.write(0x0a);
+	i += 0x20;
+}
+
+void StopTakePhotoCmd()
+{
+	Serial2.write(0x56);
+	Serial2.write(ZERO);
+	Serial2.write(0x36);
+	Serial2.write(0x01);
+	Serial2.write(0x03);
 }
